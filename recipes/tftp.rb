6cfc338cefbfd03c25cd281247eb2917322fc885
Myles Steinhauser <myles.steinhauser@gmail.com>
2014-01-31 22:22:34 -0500

    Proper tftp user

--- recipes/tftp.rb
+++ recipes/tftp.rb
@@ -3,13 +3,13 @@
 include_recipe "tftp::server"
 
 cookbook_file "undionly.kpxe" do
-  owner node[:razor][:user]
+  owner [:tftp][:username]
   group node[:razor][:group]
   path "#{node[:tftp][:directory]}/undionly.kpxe"
 end
 
 template "bootstrap.ipxe" do
-  owner node[:razor][:user]
+  owner [:tftp][:username]
   group node[:razor][:group]
   path "#{node[:tftp][:directory]}/bootstrap.ipxe"
 end
