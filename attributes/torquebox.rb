12510616e79a4e59a1c886ed02ab1854258e9de5
Myles Steinhauser <myles.steinhauser@gmail.com>
2014-02-04 21:08:17 -0500

    Downgrade torquebox to 3.0.1 for jruby 1.7.8

--- attributes/torquebox.rb
+++ attributes/torquebox.rb
@@ -2,8 +2,8 @@
 # Cookbook Name:: razor-server
 # Attributes:: torquebox
 
-node.default[:razor][:torquebox][:version] = "3.0.2"
-node.default[:razor][:torquebox][:checksum] = "a40abe4a7e71c65c599bed3c3717f5aca5f898dbb683c3940e1ca404eabf2b56"
+node.default[:razor][:torquebox][:version] = "3.0.1"
+node.default[:razor][:torquebox][:checksum] = "fd385d44619cb56d4c076c67243b0dfc4dab30145226d7f584517f1a8891cb18"
 node.default[:razor][:torquebox][:url]  = "http://torquebox.org/release/org/torquebox/torquebox-dist/#{node[:razor][:torquebox][:version]}/torquebox-dist-#{node[:razor][:torquebox][:version]}-bin.zip"
 node.default[:razor][:torquebox][:root] = "torquebox-#{node[:razor][:torquebox][:version]}"
 node.default[:razor][:torquebox][:base] = '/opt'
