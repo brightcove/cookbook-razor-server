e8c93f45c77b1280e3e7cbb7cd4feb2421688cb2
Myles Steinhauser <myles.steinhauser@gmail.com>
2014-06-05 15:18:20 +0200

    Remove Brightcove specific chefspec

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -1,28 +1,10 @@
 require_relative '../spec_helper'
-require_relative '../../../chef_helpers/libraries/encrypted_data_bag'
-include Brightcove::Helpers::EncryptedDataBag
 
 describe 'razor_server::default' do
   subject { ChefSpec::Runner.new.converge(described_recipe) }
   let(:chef_run) do
-    ChefSpec::Runner.new(platform: 'centos', version: '5.9') do |node|
-      node.set[:datacenter][:domain] = 'local'
+    ChefSpec::Runner.new(platform: 'centos', version: '5.10') do |node|
     end.converge(described_recipe)
-  end
-
-  before(:each) do
-
-    bind9_databag = {
-      'local' => {
-	'internal'    => "dummy",
-	'rndc_secret' => "dummy",
-	'external'    => "dummy"
-      }
-    }
-
-    #stub_data_bag_item("highly_confidential", "bind9").and_return(bind9_databag)
-
-    Chef::Recipe.any_instance.stub(:encrypted_data_bag_item).with("highly_confidential","bind9").and_return(bind9_databag)
   end
 
   it 'converges the node' do
