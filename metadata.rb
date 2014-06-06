6d4341d21fb5b5d2cee254e8628f88eed9da9ece
Myles Steinhauser <myles.steinhauser@gmail.com>
2014-06-06 12:54:31 -0400

    NGINX needs CORS options in location definition

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'myles.steinhasuer@gmail.com'
 license          'Apache 2.0 License'
 description      'Installs/Configures puppetlabs/razor-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.2'
+version          '0.3.3'
 
 %w{ ubuntu }.each do |os|
   supports os
